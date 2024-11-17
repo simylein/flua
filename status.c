eaeb347c8101b32e1cbb066b4390534d94bd1065
simylein <67859356+simylein@users.noreply.github.com>
2024-11-17 14:28:36 +0100

    fix(status): add internal server error

--- status.c
+++ status.c
@@ -58,6 +58,8 @@ char *status_text(int status) {
 		return "Too Many Requests";
 	case 431:
 		return "Request Header Fields Too Large";
+	case 500:
+		return "Internal Server Error";
 	case 501:
 		return "Not Implemented";
 	case 502:
