98b80c7f597975d7a021734d74d1acf45bd90dd1
simylein <67859356+simylein@users.noreply.github.com>
2024-11-18 11:22:22 +0100

    fix(file): set body length after mem copy

--- file.c
+++ file.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 void file(const char *file_path, Response *response) {
-	char buffer[6144] = {0};
+	char buffer[sizeof(response->body)] = {0};
 
 	int file_fd = open(file_path, O_RDONLY);
 	if (file_fd == -1) {
@@ -21,29 +21,29 @@ void file(const char *file_path, Response *response) {
 	if (fstat(file_fd, &file_stat) == -1) {
 		error("%s\n", errno_str());
 		error("failed to stat %s\n", file_path);
-		close(file_fd);
 		response->status = 500;
-		return;
+		goto cleanup;
 	}
 
 	if ((size_t)file_stat.st_size > sizeof(buffer)) {
 		error("file %s exceeds buffer\n", file_path);
-		close(file_fd);
 		response->status = 500;
-		return;
+		goto cleanup;
 	}
 
 	ssize_t bytes_read = read(file_fd, buffer, (size_t)file_stat.st_size);
 	if (bytes_read != file_stat.st_size) {
 		error("%s\n", errno_str());
 		error("failed to read %s\n", file_path);
-		close(file_fd);
 		response->status = 500;
-		return;
+		goto cleanup;
 	}
 
 	trace("sending file %s\n", file_path);
 	sprintf(response->header, "content-type:text/html\r\ncontent-length:%ld\r\n\r\n", bytes_read);
 	memcpy(response->body, buffer, bytes_read);
+	response->body_len += (size_t)bytes_read;
+
+cleanup:
 	close(file_fd);
 }
