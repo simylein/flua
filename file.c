774f0f4a23a4e1216dc12141bad83d9727bfb783
simylein <67859356+simylein@users.noreply.github.com>
2024-11-26 11:07:37 +0100

    fix(file): only cache successful status code on clients

--- file.c
+++ file.c
@@ -64,7 +64,9 @@ void file(const char *file_path, Response *response) {
 	}
 	response->header_len = (size_t)sprintf(response->header, "content-type:%s\r\n", type(file_path));
 	response->header_len = (size_t)sprintf(response->header, "content-length:%zu\r\n", bytes_read);
-	response->header_len = (size_t)sprintf(response->header, "cache-control:max-age=%d\r\n\r\n", cache_ttl);
+	if (response->status >= 200 && response->status <= 299) {
+		response->header_len = (size_t)sprintf(response->header, "cache-control:max-age=%d\r\n\r\n", cache_ttl);
+	}
 	response->body_len += (size_t)bytes_read;
 
 cleanup:
