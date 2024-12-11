40564dd91b9efc18501919cded80403a0982ad28
simylein <67859356+simylein@users.noreply.github.com>
2024-12-11 17:00:22 +0100

    feat(utils): strnfind for searching a buffer by length

--- utils.c
+++ utils.c
@@ -22,6 +22,49 @@ void null_init(request_t *request, response_t *response) {
 	response->body_len = 0;
 }
 
+int strnfind(char *buffer, const size_t buffer_len, const char *prefix, const char *suffix, char **string, size_t *string_len,
+						 size_t string_max_len) {
+	size_t prefix_ind = 0;
+	size_t prefix_len = strlen(prefix);
+
+	size_t suffix_ind = 0;
+	size_t suffix_len = strlen(suffix);
+
+	size_t index = 0;
+
+	while (prefix_ind < prefix_len && index < buffer_len) {
+		if (buffer[index] == prefix[prefix_ind]) {
+			prefix_ind++;
+		} else {
+			prefix_ind = 0;
+		}
+		index++;
+	}
+	if (prefix_ind != prefix_len) {
+		return -1;
+	}
+
+	*string = &buffer[index];
+	*string_len = 0;
+
+	while ((suffix_len == 0 || suffix_ind < suffix_len) && index < buffer_len) {
+		if (buffer[index] == suffix[suffix_ind]) {
+			suffix_ind++;
+		} else {
+			if (*string_len + 1 > string_max_len) {
+				break;
+			}
+			(*string_len)++;
+		}
+		index++;
+	}
+	if (suffix_ind != suffix_len) {
+		return -1;
+	}
+
+	return 0;
+}
+
 char *strcasestr(const char *buffer, const char *buf) {
 	size_t buffer_len = strlen(buffer);
 	size_t buf_len = strlen(buf);
--- utils.h
+++ utils.h
@@ -4,5 +4,8 @@
 
 void null_init(request_t *request, response_t *response);
 
+int strnfind(const char *buffer, const size_t buffer_len, const char *prefix, const char *suffix, char **string,
+						 size_t *string_len, size_t string_max_len);
+
 char *strcasestr(const char *buffer, const char *buf);
 char *strncasestrn(const char *buffer, size_t buffer_len, const char *buf, size_t buf_len);
