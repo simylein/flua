d1a166cd74b9bb2b6b77e336aee96cffffeb6ce7
simylein <67859356+simylein@users.noreply.github.com>
2024-12-16 08:59:03 +0100

    feat(user): api endpoint for finding yourself

--- user.c
+++ user.c
@@ -107,6 +107,51 @@ cleanup:
 	return status;
 }
 
+void find_user(sqlite3 *database, bwt_t *bwt, response_t *response) {
+	sqlite3_stmt *stmt;
+
+	const char *sql = "select id, username, public from user where id = ?";
+
+	if (sqlite3_prepare_v2(database, sql, -1, &stmt, NULL) != SQLITE_OK) {
+		error("%s\n", sqlite3_errmsg(database));
+		error("failed to prepare statement\n");
+		response->status = 500;
+		goto cleanup;
+	}
+
+	sqlite3_bind_blob(stmt, 1, bwt->id, sizeof(bwt->id), SQLITE_STATIC);
+
+	int result = sqlite3_step(stmt);
+	if (result == SQLITE_ROW) {
+		const uint8_t *id = sqlite3_column_blob(stmt, 0);
+		const size_t id_len = (size_t)sqlite3_column_bytes(stmt, 0);
+		const uint8_t *username = sqlite3_column_text(stmt, 1);
+		const size_t username_len = (size_t)sqlite3_column_bytes(stmt, 1);
+		const uint8_t public = (uint8_t)sqlite3_column_int(stmt, 2);
+		append_body(response, id, id_len);
+		append_body(response, username, username_len);
+		append_body(response, &public, sizeof(public));
+	} else if (result == SQLITE_DONE) {
+		error("user %.8x not found\n", *(uint32_t *)bwt->id);
+		response->status = 500;
+		goto cleanup;
+	} else {
+		error("%s\n", sqlite3_errmsg(database));
+		error("failed to execute statement\n");
+		response->status = 500;
+		goto cleanup;
+	}
+
+	info("user %.8x found himself\n", *(uint32_t *)bwt->id);
+	response->status = 200;
+
+	append_header(response, "content-type:application/octet-stream\r\n");
+	append_header(response, "content-length:%zu\r\n", response->body_len);
+
+cleanup:
+	sqlite3_finalize(stmt);
+}
+
 void delete_user(sqlite3 *database, bwt_t *bwt, response_t *response) {
 	sqlite3_stmt *stmt;
 
