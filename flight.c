61b676e408d4344e8b2dd3c3c57bcc3805264a34
simylein <67859356+simylein@users.noreply.github.com>
2025-02-06 10:54:12 +0100

    feat(flight): support additional columns in find flights

--- flight.c
+++ flight.c
@@ -59,7 +59,13 @@ cleanup:
 void find_flights(sqlite3 *database, uint8_t (*user_id)[16], char *year, size_t year_len, response_t *response) {
 	sqlite3_stmt *stmt;
 
-	const char *sql = "select starts_at, ends_at, altitude_bins, thermal_bins, speed_bins, glide_bins from flight "
+	const char *sql = "select "
+										"starts_at, ends_at, "
+										"altitude_bins, altitude_min, altitude_max, "
+										"thermal_bins, max_climb, max_sink, "
+										"speed_bins, speed_avg, speed_max, "
+										"glide_bins, distance_flown "
+										"from flight "
 										"where user_id = ? and strftime('%Y', datetime(starts_at, 'unixepoch')) = ? "
 										"order by starts_at desc";
 
@@ -80,12 +86,19 @@ void find_flights(sqlite3 *database, uint8_t (*user_id)[16], char *year, size_t 
 			const uint64_t ends_at = (uint64_t)sqlite3_column_int64(stmt, 1);
 			const uint16_t(*altitude_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 2);
 			const size_t altitude_bins_len = (size_t)sqlite3_column_bytes(stmt, 2);
-			const uint16_t(*thermal_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 3);
-			const size_t thermal_bins_len = (size_t)sqlite3_column_bytes(stmt, 3);
-			const uint16_t(*speed_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 4);
-			const size_t speed_bins_len = (size_t)sqlite3_column_bytes(stmt, 4);
-			const uint16_t(*glide_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 5);
-			const size_t glide_bins_len = (size_t)sqlite3_column_bytes(stmt, 5);
+			const uint16_t altitude_min = (uint16_t)sqlite3_column_int(stmt, 3);
+			const uint16_t altitude_max = (uint16_t)sqlite3_column_int(stmt, 4);
+			const uint16_t(*thermal_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 5);
+			const size_t thermal_bins_len = (size_t)sqlite3_column_bytes(stmt, 5);
+			const uint8_t max_climb = (uint8_t)sqlite3_column_int(stmt, 6);
+			const uint8_t max_sink = (uint8_t)sqlite3_column_int(stmt, 7);
+			const uint16_t(*speed_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 8);
+			const size_t speed_bins_len = (size_t)sqlite3_column_bytes(stmt, 8);
+			const uint16_t speed_avg = (uint16_t)sqlite3_column_int(stmt, 9);
+			const uint16_t speed_max = (uint16_t)sqlite3_column_int(stmt, 10);
+			const uint16_t(*glide_bins)[5] = (const uint16_t(*)[5])sqlite3_column_blob(stmt, 11);
+			const size_t glide_bins_len = (size_t)sqlite3_column_bytes(stmt, 11);
+			const uint32_t distance_flown = (uint32_t)sqlite3_column_int(stmt, 12);
 			if (altitude_bins_len != sizeof(*altitude_bins)) {
 				error("altitude bins length %zu does not match buffer length %zu\n", altitude_bins_len, sizeof(*altitude_bins));
 				response->status = 500;
@@ -114,6 +127,9 @@ void find_flights(sqlite3 *database, uint8_t (*user_id)[16], char *year, size_t 
 			}
 			const uint64_t n_starts_at = htonll(starts_at);
 			const uint64_t n_ends_at = htonll(ends_at);
+			const uint16_t n_altitude_min = htons(altitude_min);
+			const uint16_t n_altitude_max = htons(altitude_max);
+			const uint32_t n_distance_flown = htonl(distance_flown);
 			const uint8_t starts_at_bytes = significant_bytes(starts_at);
 			const uint8_t ends_at_bytes = significant_bytes(ends_at);
 			const uint8_t altitude_bins_bytes[5] = {
@@ -121,6 +137,8 @@ void find_flights(sqlite3 *database, uint8_t (*user_id)[16], char *year, size_t 
 					significant_bytes((*altitude_bins)[2]), significant_bytes((*altitude_bins)[3]),
 					significant_bytes((*altitude_bins)[4]),
 			};
+			const uint16_t altitude_min_bytes = significant_bytes(altitude_min);
+			const uint16_t altitude_max_bytes = significant_bytes(altitude_max);
 			const uint8_t thermal_bins_bytes[5] = {
 					significant_bytes((*thermal_bins)[0]), significant_bytes((*thermal_bins)[1]), significant_bytes((*thermal_bins)[2]),
 					significant_bytes((*thermal_bins)[3]), significant_bytes((*thermal_bins)[4]),
@@ -133,35 +151,48 @@ void find_flights(sqlite3 *database, uint8_t (*user_id)[16], char *year, size_t 
 					significant_bytes((*glide_bins)[0]), significant_bytes((*glide_bins)[1]), significant_bytes((*glide_bins)[2]),
 					significant_bytes((*glide_bins)[3]), significant_bytes((*glide_bins)[4]),
 			};
+			const uint8_t distance_flown_bytes = significant_bytes(distance_flown);
 			const uint64_t leading_bytes = ((uint64_t)starts_at_bytes << 60) | ((uint64_t)ends_at_bytes << 56) |
 																		 ((uint64_t)altitude_bins_bytes[0] << 54) | ((uint64_t)altitude_bins_bytes[1] << 52) |
 																		 ((uint64_t)altitude_bins_bytes[2] << 50) | ((uint64_t)altitude_bins_bytes[3] << 48) |
-																		 ((uint64_t)altitude_bins_bytes[4] << 46) | ((uint64_t)thermal_bins_bytes[0] << 44) |
-																		 ((uint64_t)thermal_bins_bytes[1] << 42) | ((uint64_t)thermal_bins_bytes[2] << 40) |
-																		 ((uint64_t)thermal_bins_bytes[3] << 38) | ((uint64_t)thermal_bins_bytes[4] << 36) |
-																		 ((uint64_t)speed_bins_bytes[0] << 34) | ((uint64_t)speed_bins_bytes[1] << 32) |
-																		 ((uint64_t)speed_bins_bytes[2] << 30) | ((uint64_t)speed_bins_bytes[3] << 28) |
-																		 ((uint64_t)speed_bins_bytes[4] << 26) | ((uint64_t)glide_bins_bytes[0] << 24) |
-																		 ((uint64_t)glide_bins_bytes[1] << 22) | ((uint64_t)glide_bins_bytes[2] << 20) |
-																		 ((uint64_t)glide_bins_bytes[3] << 18) | ((uint64_t)glide_bins_bytes[4] << 16);
+																		 ((uint64_t)altitude_bins_bytes[4] << 46) | ((uint64_t)altitude_min_bytes << 44) |
+																		 ((uint64_t)altitude_max_bytes << 42) | ((uint64_t)thermal_bins_bytes[0] << 40) |
+																		 ((uint64_t)thermal_bins_bytes[1] << 38) | ((uint64_t)thermal_bins_bytes[2] << 36) |
+																		 ((uint64_t)thermal_bins_bytes[3] << 34) | ((uint64_t)thermal_bins_bytes[4] << 32) |
+																		 ((uint64_t)speed_bins_bytes[0] << 30) | ((uint64_t)speed_bins_bytes[1] << 28) |
+																		 ((uint64_t)speed_bins_bytes[2] << 26) | ((uint64_t)speed_bins_bytes[3] << 24) |
+																		 ((uint64_t)speed_bins_bytes[4] << 22) | ((uint64_t)glide_bins_bytes[0] << 20) |
+																		 ((uint64_t)glide_bins_bytes[1] << 18) | ((uint64_t)glide_bins_bytes[2] << 16) |
+																		 ((uint64_t)glide_bins_bytes[3] << 14) | ((uint64_t)glide_bins_bytes[4] << 12) |
+																		 ((uint64_t)distance_flown_bytes << 9);
 			const uint64_t n_leading_bytes = htonll(leading_bytes);
-			append_body(response, &n_leading_bytes, sizeof(leading_bytes) - 2);
-			append_body(response, ((uint8_t *)&n_starts_at) + (8 - starts_at_bytes), starts_at_bytes);
-			append_body(response, ((uint8_t *)&n_ends_at) + (8 - ends_at_bytes), ends_at_bytes);
+			append_body(response, &n_leading_bytes, sizeof(leading_bytes) - 1);
+			append_body(response, ((uint8_t *)&n_starts_at) + (sizeof(n_starts_at) - starts_at_bytes), starts_at_bytes);
+			append_body(response, ((uint8_t *)&n_ends_at) + (sizeof(n_ends_at) - ends_at_bytes), ends_at_bytes);
 			for (uint8_t index = 0; index < 5; index++) {
-				append_body(response, ((uint8_t *)&(*altitude_bins)[index]) + (2 - altitude_bins_bytes[index]),
+				append_body(response, ((uint8_t *)&(*altitude_bins)[index]) + (sizeof(*altitude_bins[0]) - altitude_bins_bytes[index]),
 										altitude_bins_bytes[index]);
 			}
+			append_body(response, ((uint8_t *)&n_altitude_min) + (sizeof(n_altitude_min) - altitude_min_bytes), altitude_min_bytes);
+			append_body(response, ((uint8_t *)&n_altitude_max) + (sizeof(n_altitude_max) - altitude_max_bytes), altitude_max_bytes);
 			for (uint8_t index = 0; index < 5; index++) {
-				append_body(response, ((uint8_t *)&(*thermal_bins)[index]) + (2 - thermal_bins_bytes[index]),
+				append_body(response, ((uint8_t *)&(*thermal_bins)[index]) + (sizeof(*thermal_bins[0]) - thermal_bins_bytes[index]),
 										thermal_bins_bytes[index]);
 			}
+			append_body(response, &max_climb, sizeof(max_climb));
+			append_body(response, &max_sink, sizeof(max_sink));
 			for (uint8_t index = 0; index < 5; index++) {
-				append_body(response, ((uint8_t *)&(*speed_bins)[index]) + (2 - speed_bins_bytes[index]), speed_bins_bytes[index]);
+				append_body(response, ((uint8_t *)&(*speed_bins)[index]) + (sizeof(*speed_bins[0]) - speed_bins_bytes[index]),
+										speed_bins_bytes[index]);
 			}
+			append_body(response, &speed_avg, sizeof(speed_avg));
+			append_body(response, &speed_max, sizeof(speed_max));
 			for (uint8_t index = 0; index < 5; index++) {
-				append_body(response, ((uint8_t *)&(*glide_bins)[index]) + (2 - glide_bins_bytes[index]), glide_bins_bytes[index]);
+				append_body(response, ((uint8_t *)&(*glide_bins)[index]) + (sizeof(*glide_bins[0]) - glide_bins_bytes[index]),
+										glide_bins_bytes[index]);
 			}
+			append_body(response, ((uint8_t *)&n_distance_flown) + (sizeof(n_distance_flown) - distance_flown_bytes),
+									distance_flown_bytes);
 			rows++;
 		} else if (result == SQLITE_DONE) {
 			response->status = 200;
@@ -186,7 +217,8 @@ cleanup:
 void create_flight(sqlite3 *database, bwt_t *bwt, flight_t *flight, response_t *response) {
 	sqlite3_stmt *stmt;
 
-	const char *sql = "insert into flight (id, hash, "
+	const char *sql = "insert into flight "
+										"(id, hash, "
 										"starts_at, ends_at, "
 										"altitude_bins, altitude_min, altitude_max, "
 										"thermal_bins, max_climb, max_sink, "
