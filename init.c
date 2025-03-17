5faf73ef7ef0a5c26700afafb161cb4d8aa551d2
simylein <67859356+simylein@users.noreply.github.com>
2025-03-17 19:43:20 +0100

    feat(init): expand database schema with friend table

--- init.c
+++ init.c
@@ -42,6 +42,46 @@ cleanup:
 	return status;
 }
 
+int friend_init(sqlite3 *database) {
+	int status;
+
+	sqlite3_stmt *stmt;
+
+	const char *sql = "create table friend"
+										"(id blob not null, "
+										"user_id blob not null, "
+										"foreign key (id) references user(id) on delete cascade, "
+										"foreign key (user_id) references user(id) on delete cascade)";
+	debug("%s\n", sql);
+
+	if (sqlite3_prepare_v2(database, sql, -1, &stmt, NULL) != SQLITE_OK) {
+		error("failed to prepare statement because %s\n", sqlite3_errmsg(database));
+		status = -1;
+		goto cleanup;
+	}
+
+	if (sqlite3_step(stmt) != SQLITE_DONE) {
+		error("failed to execute statement because %s\n", sqlite3_errmsg(database));
+		status = -1;
+		goto cleanup;
+	}
+
+	char *message = NULL;
+	if (sqlite3_exec(database, "create index index_id_user_id on friend(id, user_id)", NULL, NULL, &message)) {
+		error("failed to index friend table because %s\n", message);
+		sqlite3_free(message);
+		status = -1;
+		goto cleanup;
+	};
+
+	info("successfully initialised friend table\n");
+	status = 0;
+
+cleanup:
+	sqlite3_finalize(stmt);
+	return status;
+}
+
 int flight_init(sqlite3 *database) {
 	int status;
 
@@ -97,6 +137,9 @@ cleanup:
 
 int init(sqlite3 *database) {
 	if (user_init(database) != 0) {
+		return -1;
+	}
+	if (friend_init(database) != 0) {
 		return -1;
 	}
 	if (flight_init(database) != 0) {
